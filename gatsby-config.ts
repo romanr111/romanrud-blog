25894b1d112a031718e1153676c2679508186149
Roman <romanrud@cloud.upwork.com>
2025-03-13 20:27:31 +0200

    fixed bug with posts not loading

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,15 +16,15 @@ const config: GatsbyConfig = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
     // Used for the title template on pages other than the index site
-    siteTitle: 'site_headline',
+    siteTitle: 'Roman Rud',
     // Default title of the page
-    siteTitleAlt: 'site_headline',
+    siteTitleAlt: 'Roman Rud | Personal Blog',
     // Can be used for e.g. JSONLD
-    siteHeadline: 'site_headline',
+    siteHeadline: 'Roman Rud | Personal Blog',
     // Will be used to generate absolute URLs for og:image etc.
     siteUrl: 'https://romanrud.com',
     // Used for SEO
-    siteDescription: 'seo_site_description',
+    siteDescription: 'Personal blog of Roman Rud featuring articles on technology, books, and more',
     // Will be set on the <html /> tag
     siteLanguage: 'en',
     // Used for og:image and must be placed inside the 'static' folder
